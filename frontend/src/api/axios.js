312d2763b9d60737523e6657d190fdd544dccc26
Arihant Sogani <arihant27.sogani@gmail.com>
2025-07-01 16:03:12 +0530

    changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,13 +3,13 @@ import axios from 'axios'
 export default axios.create({
     baseURL: process.env.REACT_APP_API_URL, 
     headers: { 'Content-Type': 'application/json' },
-    withCredentials: false
+    withCredentials: true
 }) 
 
 export const axiosPrivate = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     headers: { 'Content-Type': 'application/json' },
-    withCredentials: false
+    withCredentials: true
 }) 
 
 export const axiosPublic = axios.create({
