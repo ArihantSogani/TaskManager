d235e19f4e33cd12b75b73755fc4df174a94acf4
Arihant Sogani <arihant27.sogani@gmail.com>
2025-07-01 11:08:44 +0530

    changes

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -11,6 +11,7 @@ const allowedOrigins = [
   'http://192.168.1.3:3000',
   'http://localhost:4000',
   'http://127.0.0.1:4000',
+  'https://api.render.com/deploy/srv-d1ejt395pdvs73c5rfvg?key=LQoCI82Nsm8'
   // Add any other addresses you use
 ];
 const paths = ['/api/auth/google', '/api/auth/google/callback'];
